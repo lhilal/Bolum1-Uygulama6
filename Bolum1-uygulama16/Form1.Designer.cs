3f3827a87c26e0e9a8ab90675823949c0ecf7b3e
lhilal <145124346+lhilal@users.noreply.github.com>
2023-09-28 20:26:53 +0300

    Add files via upload

---
+++ Bolum1-uygulama16/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Bolum1_uygulama16
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tdogru = new System.Windows.Forms.TextBox();
+            this.tyanlis = new System.Windows.Forms.TextBox();
+            this.tnet = new System.Windows.Forms.TextBox();
+            this.mdogru = new System.Windows.Forms.TextBox();
+            this.myanlis = new System.Windows.Forms.TextBox();
+            this.mnet = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.mnet);
+            this.groupBox1.Controls.Add(this.myanlis);
+            this.groupBox1.Controls.Add(this.mdogru);
+            this.groupBox1.Controls.Add(this.tnet);
+            this.groupBox1.Controls.Add(this.tyanlis);
+            this.groupBox1.Controls.Add(this.tdogru);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(41, 43);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(328, 163);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sınav 1.Oturum";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(147, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Doğru";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(213, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Yanlış";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(277, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Net";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(17, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Türkçe(40)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(18, 103);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Matematik(40)";
+            // 
+            // tdogru
+            // 
+            this.tdogru.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tdogru.Location = new System.Drawing.Point(150, 50);
+            this.tdogru.Name = "tdogru";
+            this.tdogru.Size = new System.Drawing.Size(33, 23);
+            this.tdogru.TabIndex = 5;
+            // 
+            // tyanlis
+            // 
+            this.tyanlis.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tyanlis.Location = new System.Drawing.Point(216, 50);
+            this.tyanlis.Name = "tyanlis";
+            this.tyanlis.Size = new System.Drawing.Size(33, 23);
+            this.tyanlis.TabIndex = 6;
+            // 
+            // tnet
+            // 
+            this.tnet.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tnet.Location = new System.Drawing.Point(274, 50);
+            this.tnet.Name = "tnet";
+            this.tnet.Size = new System.Drawing.Size(33, 23);
+            this.tnet.TabIndex = 7;
+            // 
+            // mdogru
+            // 
+            this.mdogru.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.mdogru.Location = new System.Drawing.Point(150, 100);
+            this.mdogru.Name = "mdogru";
+            this.mdogru.Size = new System.Drawing.Size(33, 23);
+            this.mdogru.TabIndex = 8;
+            // 
+            // myanlis
+            // 
+            this.myanlis.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.myanlis.Location = new System.Drawing.Point(216, 100);
+            this.myanlis.Name = "myanlis";
+            this.myanlis.Size = new System.Drawing.Size(33, 23);
+            this.myanlis.TabIndex = 9;
+            // 
+            // mnet
+            // 
+            this.mnet.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.mnet.Location = new System.Drawing.Point(274, 100);
+            this.mnet.Name = "mnet";
+            this.mnet.Size = new System.Drawing.Size(33, 23);
+            this.mnet.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(164, 130);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "NET HESAPLA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(573, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox mnet;
+        private System.Windows.Forms.TextBox myanlis;
+        private System.Windows.Forms.TextBox mdogru;
+        private System.Windows.Forms.TextBox tnet;
+        private System.Windows.Forms.TextBox tyanlis;
+        private System.Windows.Forms.TextBox tdogru;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Bolum1-uygulama16/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Bolum1_uygulama16
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double turkceDogru, matDogru;
+            double turkceYanlis, matYanlis;
+            double turkceNet, matNet;
+            turkceDogru = Convert.ToDouble(tdogru.Text);
+            turkceYanlis = Convert.ToDouble(tyanlis.Text);
+            turkceNet = (turkceDogru - (turkceYanlis / 4));
+            tnet.Text = turkceNet.ToString();
+            matDogru = Convert.ToDouble(mdogru.Text);
+            matYanlis = Convert.ToDouble(myanlis.Text);
+            matNet = (matDogru - (matYanlis / 4));
+            mnet.Text = matNet.ToString();
+        }
+    }
+}
